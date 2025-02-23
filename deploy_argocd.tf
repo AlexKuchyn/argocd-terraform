4c1e455fa26387d09bb9038775195d7abf285c11
Alex_K <33528960+AlexKuchyn@users.noreply.github.com>
2025-02-22 21:30:40 -0800

    Update deploy_argocd.tf

--- deploy_argocd.tf
+++ deploy_argocd.tf
@@ -1,3 +1,5 @@
+# this part installs ArgoCD into clusters
+
 module "argocd-dev" {
   source           = "./terraform_argocd_helmchart_eks"
   eks_cluster_name = "alex-dev"
@@ -8,4 +10,22 @@ module "argocd-test" {
   source           = "./terraform_argocd_helmchart_eks"
   eks_cluster_name = "alex-test"
   chart_version    = "5.39.1"
+}
+
+# -----------------------------------------------------------------------
+# this part installs ArgoCD root applications into clusters
+# Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd-dev]
+module "argocd_dev_root_app" {
+  source             = "./terraform_argocd_rootapp_eks"
+  eks_cluster_name   = "alex-dev"
+  git_source_path    = "alex-dev/applications"
+  git_source_repoURL = "git@github.com:AlexKuchyn/argocd.git"
+}
+
+# Can be deployed ONLY after ArgoCD deployment: depends_on = [module.argocd-test]
+module "argocd_test_root_app" {
+  source             = "./terraform_argocd_rootapp_eks"
+  eks_cluster_name   = "alex-test"
+  git_source_path    = "alex-test/applications"
+  git_source_repoURL = "git@github.com:AlexKuchyn/argocd.git"
 }
